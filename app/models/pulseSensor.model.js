37764f87fd1b7157805312d3b103ea98e406e7d8
John Barry <john.olivierb@gmail.com>
2021-12-02 16:44:27 -0500

    Fixed wrong database name and indent 2 spaces

--- app/models/pulseSensor.model.js
+++ app/models/pulseSensor.model.js
@@ -2,23 +2,24 @@ module.exports = mongoose => {
   const Pulsesensor = mongoose.model(
     "pulsesensor",
     mongoose.Schema({
-      _id: {
+      id: {
         type: String,
         required: true
       },
-    universe: {
-      type: String,
-      required: false
-    },
-     state: {
-         type: String,
-         required: false
-     },
-     rgb:{
-      type: String,
-      required: false,
-      default: 'rgb(0,0,0)'
-     },
+      universe: {
+        type: String,
+        required: false
+      },
+      state: {
+        type: Number,
+        required: false,
+        default: 0,
+      },
+      rgb: {
+        type: String,
+        required: false,
+        default: '0, 0, 0'
+      },
     })
   );
   return Pulsesensor;
