1c3f108c3cf626c6f950ff3ed275d086e5a7fec5
John <john.olivierb@gmail.com>
2022-02-03 20:09:00 -0500

    Switched settimeout to cron job

--- index.ts
+++ index.ts
@@ -1,51 +1,50 @@
-import express from "express";
+import express from 'express';
 const app = express();
 import ping from 'ping';
 import cors from 'cors';
 import {connect} from 'mongoose';
-import { pingLanterns } from './app/utils';
+import {pingLanterns} from './app/utils';
+var cron = require('node-cron');
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+	origin: 'http://localhost:8081'
 };
 
-var pingcfg = { 
-  timeout: 2,
-  extra: ['-i', '2']
-}
+var pingcfg = {
+	timeout: 2,
+	extra: ['-i', '2']
+};
 
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
-		
+
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
-const db = require("./app/models").db;
+const db = require('./app/models').db;
 
-run().catch(err => console.log(err));
+run().catch((err) => console.log(err));
 
 async function run(): Promise<void> {
-  
-  try {
-    await connect(db.url);
-    console.log("Connected to the database!");
-  } catch (err) {
-    console.log(err);
-    process.exit();
-  }
+	try {
+		await connect(db.url);
+		console.log('Connected to the database!');
+	} catch (err) {
+		console.log(err);
+		process.exit();
+	}
 }
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
 
-require("./app/routes/lantern.routes")(app);
-require("./app/routes/station.routes")(app);
+require('./app/routes/lantern.routes')(app);
+require('./app/routes/station.routes')(app);
 
 app.listen(PORT, async () => {
-  console.log(`Server is running on port ${PORT}.`);
-  setInterval(() => {
-    pingLanterns();
-  }, 5000);
+	console.log(`Server is running on port ${PORT}.`);
+	cron.schedule('*/5 * * * * *', async function () {
+		await pingLanterns();
+	});
 });
