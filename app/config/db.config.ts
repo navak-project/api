f42d9f6d2e8c76757d935e7f4072f9478fadbb44
John <john.olivierb@gmail.com>
2022-03-04 20:17:42 -0500

    add dot env config

--- app/config/db.config.ts
+++ app/config/db.config.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config'
+console.log("🚀 ~ file: db.config.ts ~ line 4 ~ process.env.MONGODB", process.env.MONGODB);
 export = {
-  url: "mongodb://192.168.1.209:27017/navak"
+  url: `mongodb://${process.env.MONGODB}:27017/navak`
 };
