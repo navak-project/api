c6afd8b5f91161ee30dac9dc551174bcd0ec63db
John <john.olivierb@gmail.com>
2022-03-04 20:58:09 -0500

    updated station model

--- app/models/station.model.ts
+++ app/models/station.model.ts
@@ -1,30 +1,36 @@
 module.exports = (mongoose: any) => {
-  const Station = mongoose.model(
-    "station",
-    mongoose.Schema({
-      id: {
-        type: String,
-        required: true
-      },
-      ip: {
-        type: String,
-        required: true
-      },
-      universe: {
-        type: String,
-        required: false
-      },
-      state: {
-        type: Number,
-        required: false,
-        default: 0,
-      },
-      rgb: {
-        type: String,
-        required: false,
-        default: '0,0,0,255'
-      },
-    })
-  );
-  return Station;
+	const Station = mongoose.model(
+		'station',
+		mongoose.Schema({
+			id: {
+				type: String,
+				required: true
+			},
+			ip: {
+				type: String,
+				required: false
+			},
+			universe: {
+				type: String,
+				required: false
+			},
+			state: {
+				type: Number,
+				required: false,
+				default: 0
+			},
+			rgb: {
+				type: String,
+				required: false,
+				default: '0,0,0,255'
+			},
+			metrics: 
+      {
+        type: {
+          message: {type: String, required: true, default:null },
+        },
+      }
+		})
+	);
+	return Station;
 };
