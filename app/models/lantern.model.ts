99ae4dc47b553a2acecff2b541f005de586a07bd
John Barry <john.olivierb@gmail.com>
2022-03-17 14:39:39 -0400

    added battery level and wifisignal to model

--- app/models/lantern.model.ts
+++ app/models/lantern.model.ts
@@ -1,50 +1,60 @@
 module.exports = (mongoose: any) => {
-	const Lantern = mongoose.model(
-		'lantern',
-		mongoose.Schema({
-			id: {
-				type: String,
-				required: false,
-				default: '0000'
-			},
-			hostName: {
-				type: String,
-				required: true
-			},
-			macAddress: {
-				type: String,
-				required: true
-			},
-			ipAddress: {
-				type: String,
-				required: true
-			},
-			startUniverse: {
-				type: Number,
-				required: false,
-				default: 0
-			},
-			pulse: {
-				type: Number,
-				required: false,
-				default: 0
-			},
-			rgb: {
-				type: String,
-				required: false,
-				default: '0,0,0,1'
-			},
-			status: {
-				type: Boolean,
-				required: false,
-				default: false
-			},
-			group: {
-				type: Number,
-				required: true,
-				default: 1
-			}
-		})
-	);
-	return Lantern;
+  const Lantern = mongoose.model(
+    'lantern',
+    mongoose.Schema({
+      id: {
+        type: String,
+        required: false,
+        default: '0000'
+      },
+      hostName: {
+        type: String,
+        required: true
+      },
+      macAddress: {
+        type: String,
+        required: true
+      },
+      ipAddress: {
+        type: String,
+        required: true
+      },
+      startUniverse: {
+        type: Number,
+        required: false,
+        default: 0
+      },
+      pulse: {
+        type: Number,
+        required: false,
+        default: 0
+      },
+      rgb: {
+        type: String,
+        required: false,
+        default: '0,0,0,1'
+      },
+      status: {
+        type: Boolean,
+        required: false,
+        default: false
+      },
+      group: {
+        type: Number,
+        required: true,
+        default: 1
+      },
+      battery: {
+        type: Number,
+        required: false,
+        default: 0
+      },
+      wifiSignal: {
+        type: Number,
+        required: false,
+        default: 0
+      },
+    })
+  );
+  return Lantern;
 };
