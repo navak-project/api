c8573ab59a8d106e83383ddae58686963f74c473
John Barry <john.olivierb@gmail.com>
2022-03-27 13:37:28 -0400

    removed mqtt call

--- app/controllers/lantern.controller.ts
+++ app/controllers/lantern.controller.ts
@@ -189,7 +189,6 @@ exports.updateStatus = async (req: any, res: any) => {
 		var newValues = {status: true};
 		const target = await Lantern.findOneAndUpdate(query, newValues);
     console.log(`Lantern [ID: ${target.id} | IP: ${target.ipAddress} | MAC: ${target.macAddress}] is Online!`);
-    lanterns.publish('/lanterns/update', JSON.stringify(req.body.id));
 		res.send(`Lantern ${target['ipAddress']} is Online!`);
 	} catch (error) {
 		console.log('error', error);
